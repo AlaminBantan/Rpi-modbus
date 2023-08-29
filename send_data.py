73a24480b7d2fc622f836a17429951dad2960529
AlaminBantan <143328322+AlaminBantan@users.noreply.github.com>
2023-08-29 10:12:29 +0300

    Update send_data.py

--- send_data.py
+++ send_data.py
@@ -2,18 +2,20 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email import encoders
-import csv
+import os
+import re
 
 # Email configuration
 sender_email = "bantanalamin@gmail.com"
 sender_password = "Ab!78803812"
 receiver_email = "alamin.bantan@kaust.edu.sa"
-subject = "CSV from Raspberry Pi"
+subject = "CSVs from Raspberry Pi"
 
-# Read CSV data
-csv_file_path = "/home/cdacea/Sensors_modbus/PAR_data/data_08-29-2023.csv"
-with open(csv_file_path, "r") as csv_file:
-    csv_data = csv_file.read()
+# Directory containing CSV files
+csv_directory = "/home/cdacea/Sensors_modbus/PAR_data/"
+
+# Get a list of CSV files with date format in the filename
+csv_files = [filename for filename in os.listdir(csv_directory) if re.match(r"data_\d{2}-\d{2}-\d{4}\.csv", filename)]
 
 # Create the email content
 msg = MIMEMultipart()
@@ -21,12 +23,17 @@ msg["From"] = sender_email
 msg["To"] = receiver_email
 msg["Subject"] = subject
 
-# Attach the CSV file
-attachment = MIMEBase("application", "octet-stream")
-attachment.set_payload(csv_data.encode())
-encoders.encode_base64(attachment)
-attachment.add_header("Content-Disposition", f"attachment; filename=data.csv")
-msg.attach(attachment)
+# Attach each CSV file
+for csv_filename in csv_files:
+    csv_file_path = os.path.join(csv_directory, csv_filename)
+    with open(csv_file_path, "r") as csv_file:
+        csv_data = csv_file.read()
+
+    attachment = MIMEBase("application", "octet-stream")
+    attachment.set_payload(csv_data.encode())
+    encoders.encode_base64(attachment)
+    attachment.add_header("Content-Disposition", f"attachment; filename={csv_filename}")
+    msg.attach(attachment)
 
 # Connect to the SMTP server and send the email
 with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
