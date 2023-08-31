3cb8abcd98c6f29dfeb2c191a98cc0f24beff724
AlaminBantan <143328322+AlaminBantan@users.noreply.github.com>
2023-08-31 13:23:09 +0300

    Create test_sensor.py

---
+++ test_sensor.py
@@ -0,0 +1,44 @@
+import minimalmodbus # Don't forget to import the library!!
+from time import sleep
+
+
+mb_address = 1 # Modbus address of sensor
+
+sensy_boi = minimalmodbus.Instrument('/dev/ttyUSB0',mb_address)	# Make an "instrument" object called sensy_boi (port name, slave address (in decimal))
+
+sensy_boi.serial.baudrate = 19200 				# BaudRate
+sensy_boi.serial.bytesize = 8					# Number of data bits to be requested
+sensy_boi.serial.parity = minimalmodbus.serial.PARITY_EVEN	# Parity Setting here is NONE but can be ODD or EVEN
+sensy_boi.serial.stopbits = 1					# Number of stop bits
+sensy_boi.serial.timeout  = 0.5					# Timeout time in seconds
+sensy_boi.mode = minimalmodbus.MODE_RTU				# Mode to be used (RTU or ascii mode)
+
+# Good practice to clean up before and after each execution
+sensy_boi.clear_buffers_before_each_transaction = True
+sensy_boi.close_port_after_each_call = True
+
+
+try:
+	while True:
+		
+		# ~ read_float(registeraddress: int, functioncode: int = 3, number_of_registers: int = 2, byteorder: int = 0) 
+		light32 = sensy_boi.read_float(0, 3, 2, 0) 
+	
+		
+		print("\n"*50)
+		print("Sensor Data--------------------------------")
+		print(f"Light Intensity 32 bit: {light32} umol.m^-2.s^-1")
+		print("------------------------------------------")
+		
+		
+		
+		print("")
+		print("")
+		print("")
+		sleep(30)
+	
+except KeyboardInterrupt:
+	
+	# Piece of mind close out
+	sensy_boi.serial.close()
+	print("Ports Now Closed")
