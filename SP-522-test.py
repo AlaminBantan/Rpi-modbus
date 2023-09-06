8455e90403114cc0a8a70babcf59f224b8e10379
AlaminBantan <bantanalamin@gmail.com>
2023-09-06 18:31:04 +0300

    edits

--- SP-522-test.py
+++ SP-522-test.py
@@ -4,14 +4,10 @@ from time import sleep
 
 mb_address = 15 # Modbus address of sensor
 
-Solar_sensy = minimalmodbus.Instrument('/dev/ttyUSB0',mb_address)	# Make an "instrument" object called Solar_sensy (port name, slave address (in decimal))
+Solar_sensy = minimalmodbus.Instrument('/dev/ttyUSB0', mb_address, debug=False)	# Make an "instrument" object called Solar_sensy (port name, slave address (in decimal))
 
-Solar_sensy.serial.baudrate = 9600 				# BaudRate
-Solar_sensy.serial.bytesize = 8					# Number of data bits to be requested
-Solar_sensy.serial.parity = minimalmodbus.serial.PARITY_EVEN	# Parity Setting here is NONE but can be ODD or EVEN
-Solar_sensy.serial.stopbits = 1					# Number of stop bits
-Solar_sensy.serial.timeout  = 0.5					# Timeout time in seconds
-Solar_sensy.mode = minimalmodbus.MODE_RTU				# Mode to be used (RTU or ascii mode)
+Solar_sensy.serial.baudrate = 9600 		
+
 
 # Good practice to clean up before and after each execution
 Solar_sensy.clear_buffers_before_each_transaction = True
