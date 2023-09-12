2b2a8dc31d6b1db63d42536bea2ce409c8364f55
AlaminBantan <bantanalamin@gmail.com>
2023-09-12 13:49:51 +0300

    update

--- SP-522-10.py
+++ SP-522-10.py
@@ -2,28 +2,26 @@ import minimalmodbus # Don't forget to import the library!!
 from time import sleep
 
 
-mb_address = 10 # Modbus address of sensor
 
-Solar_15 = minimalmodbus.Instrument('/dev/ttyUSB0', mb_address, debug=False)	# Make an "instrument" object called Solar_15 (port name, slave address (in decimal))
-
-Solar_15.serial.baudrate = 19200 	
+Solar_10 = minimalmodbus.Instrument('/dev/ttyUSB0', 10, debug=False)	# Make an "instrument" object called Solar_10 (port name, slave address (in decimal))
+Solar_10.serial.baudrate = 19200 	
 
 
 
 # Good practice to clean up before and after each execution
-Solar_15.clear_buffers_before_each_transaction = True
-Solar_15.close_port_after_each_call = True
+Solar_10.clear_buffers_before_each_transaction = True
+Solar_10.close_port_after_each_call = True
 
 
 try:
 	while True:
 		
 		# ~ read_float(registeraddress: int, functioncode: int = 3, number_of_registers: int = 2, byteorder: int = 0) 
-		Solar_Radiation = Solar_15.read_float(0, 3, 2, 0)
-		slave = Solar_15.read_float(16,3,2,0)
-		Baud = Solar_15.read_float(22,3,2,0)
-		Parity = Solar_15.read_float(24,3,2,0)
-		Stopbit = Solar_15.read_float(26,3,2,0)
+		Solar_Radiation = Solar_10.read_float(0, 3, 2, 0)
+		slave = Solar_10.read_float(16,3,2,0)
+		Baud = Solar_10.read_float(22,3,2,0)
+		Parity = Solar_10.read_float(24,3,2,0)
+		Stopbit = Solar_10.read_float(26,3,2,0)
 	
 		
 		print("\n"*50)
@@ -43,5 +41,5 @@ try:
 except KeyboardInterrupt:
 	
 	# Piece of mind close out
-	Solar_15.serial.close()
+	Solar_10.serial.close()
 	print("Ports Now Closed")
