2b1321eed327f78b8374d6a7f6d57771f8347da8
AlaminBantan <bantanalamin@gmail.com>
2023-09-12 09:58:49 +0300

    update

--- test.py
+++ test.py
@@ -55,6 +55,7 @@ try:
 		PAR_intensity_1 = PAR_1.read_float(0, 3, 2, 0) 
 		Slave_1 = PAR_1.read_float(16,3,2,0)
 		Baud_1 = PAR_1.read_float(22,3,2,0)
+		print(f"PAR Intensity in Zone B is: {PAR_intensity_1} umol.m^-2.s^-1")
 		sleep(1)
 except KeyboardInterrupt:
 	PAR_1.serial.close()
@@ -65,6 +66,7 @@ try:
 		PAR_intensity_2 = PAR_2.read_float(0, 3, 2, 0) 
 		Slave_2 = PAR_2.read_float(16,3,2,0)
 		Baud_2 = PAR_2.read_float(22,3,2,0)
+		print(f"PAR Intensity in Zone C is: {PAR_intensity_2} umol.m^-2.s^-1")		
 		sleep(1)
 except KeyboardInterrupt:
 	PAR_2.serial.close()
@@ -74,6 +76,7 @@ try:
 		Solar_Radiation_10 = Solar_10.read_float(0, 3, 2, 0)
 		slave_10 = Solar_10.read_float(16,3,2,0)
 		Baud_10 = Solar_10.read_float(22,3,2,0)
+		print(f"Solar radiation in zone B is: {Solar_Radiation_10} W.m^-2")
 		sleep(1)
 except KeyboardInterrupt:
 	Solar_15.serial.close()
@@ -83,27 +86,8 @@ try:
 		Solar_Radiation_15 = Solar_15.read_float(0, 3, 2, 0)
 		slave_15 = Solar_15.read_float(16,3,2,0)
 		Baud_15 = Solar_15.read_float(22,3,2,0)
-
-		print("\n"*20)
-		print("Sensor Data in Zone B:")
-		print("--------------------------------")
-		print(f"PAR Intensity in Zone B is: {PAR_intensity_1} umol.m^-2.s^-1")
-		print(f"Slave ID is: {Slave_1} and the Baud rate is {Baud_1}")
-		print(f"Solar radiation in zone B is: {Solar_Radiation_10} W.m^-2")
-		print(f"Slave Id of pyranometer in zone B is: {slave_10} and Baud rate is {Baud_10}")
-		print("Sensor Data in Zone C:")
-		print("--------------------------------")
-		print(f"PAR Intensity in Zone B is: {PAR_intensity_2} umol.m^-2.s^-1")
-		print(f"Slave ID is: {Slave_2} and the Baud rate is {Baud_2}")
 		print(f"Solar radiation in zone C is: {Solar_Radiation_15} W.m^-2")
-		print(f"Slave Id of pyranometer in zone C is: {slave_15} and Baud rate is {Baud_15}")
-		print("------------------------------------------")
-		
-		
-		
-		print("")
-		print("")
-		print("")
+
 		sleep(1)
 	
 except KeyboardInterrupt:
@@ -112,3 +96,21 @@ except KeyboardInterrupt:
 	Solar_15.serial.close()
 	Solar_10.serial.close()
 	print("Ports Now Closed")
+
+
+
+
+	
+	#	print("\n"*20)
+	#	print("Sensor Data in Zone B:")
+	#	print("--------------------------------")
+	#	print(f"PAR Intensity in Zone B is: {PAR_intensity_1} umol.m^-2.s^-1")
+	#	print(f"Slave ID is: {Slave_1} and the Baud rate is {Baud_1}")
+	#	print(f"Solar radiation in zone B is: {Solar_Radiation_10} W.m^-2")
+	#	print(f"Slave Id of pyranometer in zone B is: {slave_10} and Baud rate is {Baud_10}")
+	#	print("Sensor Data in Zone C:")
+	#	print("--------------------------------")
+	#	print(f"Slave ID is: {Slave_2} and the Baud rate is {Baud_2}")
+	#	print(f"Solar radiation in zone C is: {Solar_Radiation_15} W.m^-2")
+	#	print(f"Slave Id of pyranometer in zone C is: {slave_15} and Baud rate is {Baud_15}")
+	#	print("------------------------------------------")
