27c21a103fc253c5c7c1c5cb724b87a8f3f5cc9e
AlaminBantan <bantanalamin@gmail.com>
2023-09-13 10:02:19 +0300

    update

--- change_parity.py
+++ change_parity.py
@@ -4,13 +4,8 @@ from time import sleep
 
 
 
-Solar_10 = minimalmodbus.Instrument('/dev/ttyUSB0', 10, debug=False)	# Make an "instrument" object called Solar_10 (port name, slave address (in decimal))
+Solar_10 = minimalmodbus.Instrument('/dev/ttyUSB0', 10)	# Make an "instrument" object called Solar_10 (port name, slave address (in decimal))
 Solar_10.serial.baudrate = 19200 
-Solar_10.serial.bytesize = 8					# Number of data bits to be requested
-Solar_10.serial.stopbits = 1					# Number of stop bits
-Solar_10.serial.timeout  = 0.5					# Timeout time in seconds
-Solar_10.mode = minimalmodbus.MODE_RTU			
-
 
 
 # Good practice to clean up before and after each execution
