feb02f71396c66d14373b872f2173d0639251844
AlaminBantan <bantanalamin@gmail.com>
2023-09-13 09:51:58 +0300

    update

--- change_parity.py
+++ change_parity.py
@@ -2,16 +2,15 @@ import minimalmodbus # Don't forget to import the library!!
 from time import sleep
 
 
-mb_address = 15 # Modbus address of sensor
 
-Solar_15 = minimalmodbus.Instrument('/dev/ttyUSB0', mb_address, debug=False)	# Make an "instrument" object called Solar_15 (port name, slave address (in decimal))
 
-Solar_15.serial.baudrate = 19200 		
+Solar_10 = minimalmodbus.Instrument('/dev/ttyUSB0', 10, debug=False)	# Make an "instrument" object called Solar_10 (port name, slave address (in decimal))
+Solar_10.serial.baudrate = 19200 		
 
 
 # Good practice to clean up before and after each execution
-Solar_15.clear_buffers_before_each_transaction = True
-Solar_15.close_port_after_each_call = True
+Solar_10.clear_buffers_before_each_transaction = True
+Solar_10.close_port_after_each_call = True
 
 
-Solar_15.write_register(52,0, 0, 16, False)
+Solar_10.write_register(51,3,0,16, False)
