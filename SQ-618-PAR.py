af5a9edf6bc7b5e47d48ba53c75611c17e6fddd2
AlaminBantan <bantanalamin@gmail.com>
2023-09-14 15:23:40 +0300

    update

--- SQ-618-PAR.py
+++ SQ-618-PAR.py
@@ -3,23 +3,20 @@ from time import sleep, strftime
 import csv
 import os
 
+PAR_1 = minimalmodbus.Instrument('/dev/ttyUSB0',1)	# Make an "instrument" object called PAR_1 (port name, slave address (in decimal))
 
-mb_address = 1  # Modbus address of sensor
-
-PAR_sensy = minimalmodbus.Instrument('/dev/ttyUSB0',mb_address)	# Make an "instrument" object called PAR_sensy (port name, slave address (in decimal))
-
-PAR_sensy.serial.baudrate = 19200 				# BaudRate
-PAR_sensy.serial.bytesize = 8					# Number of data bits to be requested
-PAR_sensy.serial.parity = minimalmodbus.serial.PARITY_EVEN	# Parity Setting here is NONE but can be ODD or EVEN
-PAR_sensy.serial.stopbits = 1					# Number of stop bits
-PAR_sensy.serial.timeout  = 0.5					# Timeout time in seconds
-PAR_sensy.mode = minimalmodbus.MODE_RTU				# Mode to be used (RTU or ascii mode)
+PAR_1.serial.baudrate = 19200 	
+PAR_1.serial.bytesize = 8					# Number of data bits to be requested
+PAR_1.serial.parity = minimalmodbus.serial.PARITY_EVEN	# Parity Setting here is NONE but can be ODD or EVEN
+PAR_1.serial.stopbits = 1					# Number of stop bits
+PAR_1.serial.timeout  = 0.5					# Timeout time in seconds
+PAR_1.mode = minimalmodbus.MODE_RTU				# Mode to be used (RTU or ascii mode)
 
 # Good practice to clean up before and after each execution
-PAR_sensy.clear_buffers_before_each_transaction = True
-PAR_sensy.close_port_after_each_call = True
+PAR_1.clear_buffers_before_each_transaction = True
+PAR_1.close_port_after_each_call = True
 
-csv_header = ["Date", "Time", "PAR Intensity (umol.m^-2.s^-1)"]
+csv_header = ["Date", "Time", "PAR Intensity in zone b (umol.m^-2.s^-1)"]
 
 while True:
     # Get current date and time
@@ -33,7 +30,7 @@ while True:
             csv_writer.writerow(csv_header)
 
     # Read light intensity from the sensor
-    PAR_intensity = PAR_sensy.read_float(0, 3, 2, 0)
+    PAR_intensity = PAR_1.read_float(0, 3, 2, 0)
 
     # Append data to the current day's CSV file
     with open(f"data_{current_date}.csv", mode="a", newline='') as csv_file:
@@ -46,5 +43,5 @@ while True:
 
     sleep(60)
 
-PAR_sensy.serial.close()
+PAR_1.serial.close()
 print("Ports Now Closed")
