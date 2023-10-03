23f8b0898e83ac32b74dfc6dc8d7d307324314e3
AlaminBantan <bantanalamin@gmail.com>
2023-10-03 11:15:08 +0300

    update

--- HMP-155-20.py
+++ HMP-155-20.py
@@ -2,16 +2,8 @@ import minimalmodbus # Don't forget to import the library!!
 from time import sleep
 
 
-Thum_20 = minimalmodbus.Instrument('/dev/ttyUSB0', 240, debug=False)
-Thum_20.serial.baudrate = 4800	
-Thum_20.serial.bytesize = 7				# Number of data bits to be requested
-Thum_20.serial.parity = minimalmodbus.serial.PARITY_EVEN	# Parity Setting here is NONE but can be ODD or EVEN
-Thum_20.serial.stopbits = 1					# Number of stop bits
-
-
-# Good practice to clean up before and after each execution
-Thum_20.clear_buffers_before_each_transaction = True
-Thum_20.close_port_after_each_call = True
+Thum_20 = minimalmodbus.Instrument('/dev/ttyACM0', 240, debug=False)
+Thum_20.serial.baudrate = 19200
 
 
 try:
