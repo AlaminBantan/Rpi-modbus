0ff4d8f5b1ec304474ebe03c017b44783cd7d3f9
AlaminBantan <bantanalamin@gmail.com>
2023-10-11 17:01:45 +0300

    up

--- control.py
+++ control.py
@@ -1,6 +1,5 @@
 import RPi.GPIO as GPIO
 import time
-from datetime import datetime, time as dt_time
 
 channel = 2 
 
@@ -8,28 +7,23 @@ channel = 2
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel, GPIO.OUT)
 
+
 def relay1_on(pin):
-    GPIO.output(pin, GPIO.HIGH)  # Turn relay on
-    print(f"Relay {pin} ON")
+    GPIO.output(pin, GPIO.HIGH)  # Turn relay1 on
+
 
 def relay1_off(pin):
-    GPIO.output(pin, GPIO.LOW)  # Turn relay off
-    print(f"Relay {pin} OFF")
+    GPIO.output(pin, GPIO.LOW)  # Turn relay1 off
 
-try:
-    # Define target times
-    target_on_time = datetime.combine(datetime.today(), dt_time(16, 56))
-    target_off_time = datetime.combine(datetime.today(), dt_time(16, 58))
-    
-    while True:
-        current_time = datetime.now()
-        
-        if current_time >= target_on_time and current_time < target_off_time:
-            relay1_on(channel)
-        else:
-            relay1_off(channel)
-            
+
+if __name__ == '__main__':
+    try:
+        relay1_on(channel)
         time.sleep(1)
-        
-except KeyboardInterrupt:
-    GPIO.cleanup()
+        relay1_off(channel)
+        time.sleep(4)
+        relay1_on(channel)
+        time.sleep(1)
+        GPIO.cleanup()
+    except KeyboardInterrupt:
+        GPIO.cleanup()
