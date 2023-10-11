9381b48ca2a431b0d040b053253068766b4ce06c
AlaminBantan <bantanalamin@gmail.com>
2023-10-11 17:09:23 +0300

    upp

--- control.py
+++ control.py
@@ -7,25 +7,32 @@ channel = 2
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel, GPIO.OUT)
 
-
 def relay1_on(pin):
     GPIO.output(pin, GPIO.HIGH)  # Turn relay1 on
-
 
 def relay1_off(pin):
     GPIO.output(pin, GPIO.LOW)  # Turn relay1 off
 
-
 if __name__ == '__main__':
     try:
-        relay1_on(channel)
-        time.sleep(5)
-        relay1_off(channel)
-        time.sleep(10)
-        relay1_on(channel)
-        time.sleep(10)
-        relay1_off(channel)
-        time.sleep(10)
-        GPIO.cleanup()
+        current_time = time.localtime(time.time())
+        current_hour = current_time.tm_hour
+        current_minute = current_time.tm_min
+
+        if not (current_hour == 17 and current_minute >= 11 and current_minute <= 13):
+            relay1_on(channel)
+
+        while True:
+            time.sleep(60)  # Check time every minute
+
+            current_time = time.localtime(time.time())
+            current_hour = current_time.tm_hour
+            current_minute = current_time.tm_min
+
+            if current_hour == 17 and current_minute >= 11 and current_minute <= 13:
+                relay1_off(channel)
+            else:
+                relay1_on(channel)
+
     except KeyboardInterrupt:
         GPIO.cleanup()
