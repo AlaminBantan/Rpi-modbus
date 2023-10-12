24cb2a14fe08dd5e69c557ec03fdf142873dbf51
AlaminBantan <bantanalamin@gmail.com>
2023-10-12 11:52:39 +0300

    up

--- Control/Fan_control.py
+++ Control/Fan_control.py
@@ -21,12 +21,12 @@ try:
         current_second = current_time.tm_sec
 
         if (current_hour == 6 and current_minute >= 00 and current_second >= 45) and (current_hour < 18 and current_minute >= 00):
-            fan1_on(channel)
-            time.sleep(1140)
             fan1_off(channel)
+            time.sleep(1140)
+            fan1_on(channel)
             time.sleep(60)
         else:
-            fan1_off(channel)
+            fan1_on(channel)
             time.sleep(1)
 
 except KeyboardInterrupt:
