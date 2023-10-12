175113b1ee0c90752ffd5d437277f9d4d457b9c4
AlaminBantan <bantanalamin@gmail.com>
2023-10-12 10:37:02 +0300

    up

--- control_test.py
+++ control_test.py
@@ -15,21 +15,12 @@ def relay1_off(pin):
 
 if __name__ == '__main__':
     try:
-        current_time = time.localtime(time.time())
-        current_hour = current_time.tm_hour
-        current_minute = current_time.tm_min
-
-        # Calculate the time remaining until the first cycle at 6:20 am
-        time_until_first_cycle = (6 - current_hour) * 60 + (20 - current_minute)
-        if time_until_first_cycle > 0:
-            print(f"Waiting for {time_until_first_cycle} minutes until the first cycle starts.")
-            time.sleep(time_until_first_cycle * 60)
-
         while True:
             current_time = time.localtime(time.time())
             current_hour = current_time.tm_hour
+            current_minute = current_time.tm_min
 
-            if current_hour >= 6 and current_hour < 18:  # Changed to run from 6 am to 6 pm
+            if (current_hour == 10 and current_minute >= 39) or (current_hour > 6 and current_hour < 18):
                 relay1_off(channel)
                 time.sleep(30)
                 relay1_on(channel)
