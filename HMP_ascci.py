cf25894535af65bb3bd10e15c9cf91e725c27eb4
AlaminBantan <bantanalamin@gmail.com>
2023-10-15 11:51:31 +0300

    new

---
+++ HMP_ascci.py
@@ -0,0 +1,25 @@
+import minimalmodbus 
+from time import sleep
+
+
+Thum_240 = minimalmodbus.Instrument('/dev/ttyACM0', 240, debug=True)
+Thum_240.serial.baudrate = 4800
+Thum_240.serial.parity = minimalmodbus.serial.PARITY_EVEN
+Thum_240.serial.bytesize = 7
+Thum_240.serial.stopbits = 1
+Thum_240.mode = minimalmodbus.MODE_ASCII		
+
+try:
+	while True:
+
+		SMODE RUN
+		
+
+	
+except KeyboardInterrupt:
+	
+	# Piece of mind close out
+	Thum_240.serial.close()
+	print("Ports Now Closed")
+
+
