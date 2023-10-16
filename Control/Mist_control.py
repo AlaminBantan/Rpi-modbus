08f1265f71cdbfadcc5fbd641c1413b6cc51c30e
AlaminBantan <bantanalamin@gmail.com>
2023-10-16 14:06:45 +0300

    up

--- Control/Mist_control.py
+++ Control/Mist_control.py
@@ -6,6 +6,7 @@ channel = 2
 
 # GPIO setup
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)  # Suppress GPIO warnings
 GPIO.setup(channel, GPIO.OUT)
 
 def mist_on(pin):
@@ -21,8 +22,8 @@ try:
         current_time = datetime.now().time()
 
         # Define the start and end times
-        start_time = datetime.time(6, 20)
-        end_time = datetime.time(18, 0)
+        start_time = time(6, 20)
+        end_time = time(18, 0)
 
         # Check if the current time is between 6:20 AM and 6:00 PM
         if start_time <= current_time <= end_time:
