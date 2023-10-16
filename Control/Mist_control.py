e27a0143566fcccee49b13329e527f131db0ca69
AlaminBantan <bantanalamin@gmail.com>
2023-10-16 11:36:29 +0300

    up

--- Control/Mist_control.py
+++ Control/Mist_control.py
@@ -7,27 +7,28 @@ channel = 2
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel, GPIO.OUT)
 
-def fan1_on(pin):
+def mist_on(pin):
     GPIO.output(pin, GPIO.HIGH)  
 
-def fan1_off(pin):
+def mist_off(pin):
     GPIO.output(pin, GPIO.LOW)  
 
 try:
     while True:
-        current_time = time.localtime(time.time())
-        current_hour = current_time.tm_hour
-        current_minute = current_time.tm_min
-        current_second = current_time.tm_sec
+       current_time = time.localtime(time.time())
+       current_hour = current_time.tm_hour
+       current_minute = current_time.tm_min
+       current_second = current_time.tm_sec
 
-        if (current_hour == 6 and current_minute >= 00 and current_second >= 45) and (current_hour < 18 and current_minute >= 00):
-            fan1_on(channel)
+       if (current_hour == 6 and current_minute >= 00 and current_second >= 45) and (current_hour == 6 and current_minute < 19):
+            mist_on(channel)
             time.sleep(30)
-            fan1_off(channel)
+            mist_off(channel)
             time.sleep(1170)
-        else:
-            fan1_off(channel)
+       else:
+            mist_off(channel)
             time.sleep(1)
+
 
 except KeyboardInterrupt:
     GPIO.cleanup()
