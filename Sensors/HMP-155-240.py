d0321fc18da203353f25df37475bd71df221e005
AlaminBantan <bantanalamin@gmail.com>
2023-10-17 15:33:45 +0300

    up

--- Sensors/HMP-155-240.py
+++ Sensors/HMP-155-240.py
@@ -1,39 +1,29 @@
-import minimalmodbus 
-from time import sleep
+import serial
+import time
+import io
+import csv
+from datetime import datetime
 
-
-Thum_240 = minimalmodbus.Instrument('/dev/ttyACM0', 240, debug=True)
-Thum_240.serial.baudrate = 4800
-Thum_240.serial.parity = minimalmodbus.serial.PARITY_EVEN
-Thum_240.serial.bytesize = 7
-Thum_240.serial.stopbits = 1
-Thum_240.mode = minimalmodbus.MODE_ASCII		
-
+THUM_240 = serial.Serial("/dev/ttyACM0",
+                   baudrate=4800,
+                   bytesize=serial.SEVENBITS,
+                   parity=serial.PARITY_EVEN,
+                   stopbits=serial.STOPBITS_ONE,
+                   xonxoff=False,
+                   timeout=1)
+THUM_240 = io.TextIOWrapper(io.BufferedRWPair(THUM_240, THUM_240))
 try:
-	while True:
-		
-		# ~ read_float(registeraddress: int, functioncode: int = 3, number_of_registers: int = 2, byteorder: int = 0) 
-		Relative_humidity_240 = Thum_240.read_float(1, 3, 2, 0)
-		Temperature_240 = Thum_240.read_float(3, 3, 2, 0)
+    while True:
+        THUM_240.write("R")
+        data = THUM_240.readline()
+        
+        # Assuming the response format is "RH= 54.4 %RH Ta= 26.8 'C"
+        parts = data.split()
+        humidity = float(parts[2])  # Extract humidity value
+        temperature = float(parts[5])  # Extract temperature value
 
-	
-		
-		print("\n"*50)
-		print("Sensor Data--------------------------------")
-		print(f"Humidity is: {Relative_humidity_240} %RH and Temp is: {Temperature_240} C")
+        print(f"The humidity is {humidity} and the temperature is {temperature} °C")
 
-
-		print("------------------------------------------")
-		
-		print("")
-		print("")
-		print("")
-		sleep(1)
-	
 except KeyboardInterrupt:
-	
-	# Piece of mind close out
-	Thum_240.serial.close()
-	print("Ports Now Closed")
-
-
+    # Clean up when interrupted
+    print("Ports Now Closed")
