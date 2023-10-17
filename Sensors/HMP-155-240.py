7d7719a828791cbfc6a5d7847109271a5c76cc65
AlaminBantan <bantanalamin@gmail.com>
2023-10-17 15:47:57 +0300

    up

--- Sensors/HMP-155-240.py
+++ Sensors/HMP-155-240.py
@@ -12,6 +12,12 @@ THUM_240 = serial.Serial("/dev/ttyACM0",
                    xonxoff=False,
                    timeout=1)
 THUM_240 = io.TextIOWrapper(io.BufferedRWPair(THUM_240, THUM_240))
+
+# Open the CSV file for writing
+csv_file = open('sensor_data.csv', mode='w', newline='')
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['Date', 'Time', 'Humidity', 'Temperature'])
+
 try:
     while True:
         THUM_240.write("R")
@@ -20,16 +26,28 @@ try:
         # Assuming the response format is "RH= 54.4 %RH Ta= 26.8 'C"
         parts = data.split()
         
+        # Get current date and time
+        current_datetime = datetime.now()
+        date = current_datetime.strftime('%Y-%m-%d')
+        time = current_datetime.strftime('%H:%M:%S')
+
         # Check if the response format is correct
         if len(parts) >= 6 and parts[0] == 'RH=' and parts[2] == '%RH' and parts[3] == 'Ta=' and parts[5] == "'C":
             # Extract humidity and temperature values
             humidity = float(parts[1])
             temperature = float(parts[4])
 
-            print(f"The humidity is {humidity} and the temperature is {temperature} °C")
+            # Log data to CSV
+            csv_writer.writerow([date, time, humidity, temperature])
+
         else:
-            print("Invalid response format")
+            # Log invalid response to CSV
+            print(f"Invalid response format at {date} {time}")
+
+        # Wait for 60 seconds (1 minute)
+        time.sleep(60)
 
 except KeyboardInterrupt:
     # Clean up when interrupted
     print("Ports Now Closed")
+    csv_file.close()
