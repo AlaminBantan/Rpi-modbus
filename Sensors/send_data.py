be654493fe23ca13c0d3ae9968638e040b91d322
AlaminBantan <bantanalamin@gmail.com>
2023-10-26 14:44:27 +0300

    up

--- Sensors/send_data.py
+++ Sensors/send_data.py
@@ -9,10 +9,11 @@ subject = 'Reading of the zones'
 body = 'These are the light data from zones b and c'
 
 # Attach the file
-attachment = "Light_comparison.csv"
+attachment1 = "Light_comparison.csv"
+attachment2 = "Light_comparison11.csv"
 
 # Send the email
-yag.send(to, subject, [body, attachment])
+yag.send(to, subject, [body, attachment1, attachment2])
 
 # Logout
 yag.close()
