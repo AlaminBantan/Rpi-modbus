b776db595c48da2548c5cf0bf97197cac99e7d36
AlaminBantan <bantanalamin@gmail.com>
2023-10-29 15:53:08 +0300

    UP

--- Sensors/GMP-252-240.py
+++ Sensors/GMP-252-240.py
@@ -4,12 +4,12 @@ from time import sleep
 
 
 # Make an "instrument" object called carbo_240 (port name, slave address (in decimal))
-carbo_240 = minimalmodbus.Instrument('/dev/ttyACM0',240, debug=False)	
+carbo_240 = minimalmodbus.Instrument('/dev/ttyACM0',41, debug=False)	
 
 carbo_240.serial.baudrate = 19200 				# BaudRate
 carbo_240.serial.bytesize = 8					# Number of data bits to be requested
-carbo_240.serial.parity = minimalmodbus.serial.PARITY_NONE	# Parity Setting here is NONE but can be ODD or EVEN
-carbo_240.serial.stopbits = 2					# Number of stop bits
+carbo_240.serial.parity = minimalmodbus.serial.PARITY_EVEN	# Parity Setting here is NONE but can be ODD or EVEN
+carbo_240.serial.stopbits = 1					# Number of stop bits
 carbo_240.mode = minimalmodbus.MODE_RTU				# Mode to be used (RTU or ascii mode)
 
 # Good practice to clean up before and after each execution
