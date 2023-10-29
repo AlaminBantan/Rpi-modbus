5ce33ebd0523b41343526870af3b6e243c1850ec
AlaminBantan <bantanalamin@gmail.com>
2023-10-29 15:24:24 +0300

    up

--- Sensors/HMP_ascci.py
+++ Sensors/HMP_ascci.py
@@ -9,15 +9,16 @@ THUM_240 = serial.Serial("/dev/ttyACM0",
                    bytesize=serial.EIGHTBITS,
                    parity=serial.PARITY_EVEN,
                    stopbits=serial.STOPBITS_ONE,
-                   xonxoff=False,
-                   timeout=1)
+                   xonxoff=False)
 THUM_240 = io.TextIOWrapper(io.BufferedRWPair(THUM_240, THUM_240))
 try:
     while True:
-        THUM_240.write("send 33")
+        THUM_240.write("OPEN 33")
+        THUM_240.write("SEND 33")
         data=THUM_240.readline()
         
         print(f"{data}")
+        THUM_240.write("CLOSE 33")        
 
 except KeyboardInterrupt:
     # Clean up when interrupted
