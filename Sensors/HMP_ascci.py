595ef5d3dce012de829ac6512fda045391a05df8
AlaminBantan <bantanalamin@gmail.com>
2023-10-29 15:27:15 +0300

    up

--- Sensors/HMP_ascci.py
+++ Sensors/HMP_ascci.py
@@ -5,11 +5,12 @@ import csv
 from datetime import datetime
 
 THUM_240 = serial.Serial("/dev/ttyACM0",
-                   baudrate=19200,
-                   bytesize=serial.EIGHTBITS,
+                   baudrate=4800,
+                   bytesize=serial.SEVENBITS,
                    parity=serial.PARITY_EVEN,
                    stopbits=serial.STOPBITS_ONE,
-                   xonxoff=False)
+                   xonxoff=False,
+                   timeout=1)
 THUM_240 = io.TextIOWrapper(io.BufferedRWPair(THUM_240, THUM_240))
 try:
     while True:
