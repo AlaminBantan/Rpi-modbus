4ba61d83508c1396e7a4cdab80a09a800fd53094
AlaminBantan <bantanalamin@gmail.com>
2023-10-31 10:30:08 +0300

    up

--- Sensors/HMP_ascci.py
+++ Sensors/HMP_ascci.py
@@ -1,7 +1,6 @@
 import serial
-import time
 import io
-import csv
+
 from datetime import datetime
 
 THUM_240 = serial.Serial("/dev/ttyACM0",
@@ -14,13 +13,11 @@ THUM_240 = serial.Serial("/dev/ttyACM0",
 THUM_240 = io.TextIOWrapper(io.BufferedRWPair(THUM_240, THUM_240))
 try:
     while True:
-        THUM_240.write("OPEN 33")
-        time.sleep(1)
-        THUM_240.write("SEND 33")
+        THUM_240.write("R")
         data=THUM_240.readline()
         
         print(f"{data}")
-        THUM_240.write("CLOSE 33")        
+       
 
 except KeyboardInterrupt:
     # Clean up when interrupted
