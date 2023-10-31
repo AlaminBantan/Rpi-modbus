c4a2cb24d07228ff67977762c5688aaa7e038a45
AlaminBantan <bantanalamin@gmail.com>
2023-10-31 15:38:53 +0300

    up

--- Control/test.py
+++ Control/test.py
@@ -2,11 +2,11 @@ import RPi.GPIO as GPIO
 from datetime import datetime, time
 import time as t
 
-channel_21 = 21 #change channel_21 based on relay
+channel_16 = 16 #change channel_16 based on relay
 
 # GPIO setup
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(channel_21, GPIO.OUT)
+GPIO.setup(channel_16, GPIO.OUT)
 
 def pump_off(pin):
     GPIO.output(pin, GPIO.HIGH)  
@@ -17,10 +17,10 @@ def pump_on(pin):
 try:
     while True:
             print("GPIO is low")
-            pump_on(channel_21)
+            pump_on(channel_16)
             t.sleep(5)
             print("GPIO is low")
-            pump_off(channel_21)
+            pump_off(channel_16)
             t.sleep(5)
 except KeyboardInterrupt:
     GPIO.cleanup()
