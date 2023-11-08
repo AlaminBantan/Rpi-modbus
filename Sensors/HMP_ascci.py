78e08a5bba523f1775e2c5c39c1f9fecd3831849
AlaminBantan <bantanalamin@gmail.com>
2023-11-08 10:51:32 +0300

    up

--- Sensors/HMP_ascci.py
+++ Sensors/HMP_ascci.py
@@ -3,27 +3,27 @@ import io
 from time import sleep
 from datetime import datetime
 
-THUM_240 = serial.Serial("/dev/ttyACM0",
+THUM_34 = serial.Serial("/dev/ttyACM0",
                    baudrate=19200,
                    bytesize=serial.EIGHTBITS,
                    parity=serial.PARITY_EVEN,
                    stopbits=serial.STOPBITS_ONE,
                    xonxoff=False)
-THUM_240 = io.TextIOWrapper(io.BufferedRWPair(THUM_240, THUM_240))
+HUM_34 = io.TextIOWrapper(io.BufferedRWPair(THUM_34, THUM_34))
 try:
     while True:
         open_34 = "OPEN 34"
-        THUM_240.write(open_34)
-        THUM_240.flush()
+        HUM_34.write(open_34)
+        HUM_34.flush()
         sleep(2)
         send_34 = "SEND 34"
-        THUM_240.write(send_34)
-        data=THUM_240.readline()
+        HUM_34.write(send_34)
+        data=THUM_34.readline()
         print(f"{data}")
-        THUM_240.flush()
+        HUM_34.flush()
         sleep(2)
         close_34 = "CLOSE"
-        THUM_240.write(close_34)
+        HUM_34.write(close_34)
 
 except KeyboardInterrupt:
     # Clean up when interrupted
