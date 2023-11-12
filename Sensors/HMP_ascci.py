8d26c0c396723b73bc0af058c51abd3d05c48e71
AlaminBantan <bantanalamin@gmail.com>
2023-11-12 11:17:50 +0300

    up

--- Sensors/HMP_ascci.py
+++ Sensors/HMP_ascci.py
@@ -9,19 +9,21 @@ THUM_34 = serial.Serial("/dev/ttyACM0",
                    parity=serial.PARITY_EVEN,
                    stopbits=serial.STOPBITS_ONE,
                    xonxoff=False,
-                   timeout=1)
+                   timeout=2)
 HUM_34 = io.TextIOWrapper(io.BufferedRWPair(THUM_34, THUM_34))
 try:
     while True:
-        HUM_34.write("open 34\n")
+        HUM_34.write("open 34")
         HUM_34.flush()
-        sleep(0.5)
-        HUM_34.write("send\n")
-        data=HUM_34.readline()
+        sleep(1)
+        HUM_34.write("send")
+        HUM_34.flush()
+        sleep(1)
+        data=HUM_34.readline().decode('utf-8')
         print(f"{data}")
         HUM_34.flush()
-        sleep(0.5)
-        HUM_34.write("close\n")
+        sleep(1)
+        HUM_34.write("close")
 
 except KeyboardInterrupt:
     # Clean up when interrupted
