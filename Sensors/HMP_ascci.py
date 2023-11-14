7c6a4c9235f3b47f245e7b3628d1d914a0c2f303
AlaminBantan <bantanalamin@gmail.com>
2023-11-14 10:51:35 +0300

    up

--- Sensors/HMP_ascci.py
+++ Sensors/HMP_ascci.py
@@ -16,19 +16,17 @@ THUM_33 = io.TextIOWrapper(io.BufferedRWPair(serial_THUM, serial_THUM))
 try:
     while True:
         THUM_33.write("open 33\r\n")
-        THUM_33.flush()
         print("33 is opened")
-        THUM_33.flush()
         sleep(3)
+
         THUM_33.write("send\r\n")
-        THUM_33.flush()
         print("send")
-        THUM_33.flush()
         sleep(10)
+        
         data_33 = THUM_33.readline()
         print(f"data is: {data_33}")
-        THUM_33.flush()
         sleep(3)
+
         THUM_33.write("close\r\n")
         print("closed")
         sleep(5)
