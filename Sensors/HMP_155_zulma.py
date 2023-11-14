bb001ea13767671d0d8adfee728a95b4bb2e5a45
AlaminBantan <bantanalamin@gmail.com>
2023-11-14 14:59:40 +0300

    up

--- Sensors/HMP_155_zulma.py
+++ Sensors/HMP_155_zulma.py
@@ -1,6 +1,5 @@
 import serial
 import io
-import re
 from time import sleep
 
 def read_device(serial_wrapper, device_number):
@@ -9,31 +8,22 @@ def read_device(serial_wrapper, device_number):
         serial_wrapper.write(f"OPEN {device_number}\r\n")
         serial_wrapper.flush()
         print(f"Device {device_number} is opened")
-        sleep(3)
+        sleep(5)
 
         # Send the data request
         serial_wrapper.write("SEND\r\n")
         serial_wrapper.flush()
         print("Send")
-        sleep(3)
+        sleep(10)
 
         # Read and print the data
         data = serial_wrapper.readlines()
-
-        # Extract RH and Ta from the data
-        rh_match = re.search(r'RH= (\d+\.\d+)', data[-1])
-        ta_match = re.search(r'Ta= (\d+\.\d+)', data[-1])
-
-        if rh_match and ta_match:
-            rh_value = rh_match.group(1)
-            ta_value = ta_match.group(1)
-            print(f"Data from Device {device_number}: RH={rh_value}% Ta={ta_value}'C")
-
+        print(f"Data from Device {device_number}: {data}")
         sleep(3)
 
-    except Exception as e:
-        # Handle exceptions gracefully
-        print(f"Error in device {device_number}: {e}")
+    except KeyboardInterrupt:
+        # Handle Ctrl+C gracefully
+        print(f"Ctrl+C received. Closing Device {device_number}")
     finally:
         # Close the device
         serial_wrapper.write("CLOSE\r\n")
@@ -41,10 +31,10 @@ def read_device(serial_wrapper, device_number):
         print(f"Device {device_number} closed")
         sleep(5)
 
-# Define device numbers in a circular manner
-device_numbers = ["31", "32", "33", "34"]
+# Define device numbers for only two devices (0 and 1)
+device_numbers = ["0", "1"]
 
-# Serial configuration for all devices
+# Serial configuration for the two devices
 serial_devices = [serial.Serial("/dev/ttyACM0",
                                 baudrate=4800,
                                 bytesize=serial.SEVENBITS,
@@ -53,14 +43,13 @@ serial_devices = [serial.Serial("/dev/ttyACM0",
                                 xonxoff=False,
                                 timeout=2) for _ in range(len(device_numbers))]
 
-# TextIOWrapper objects for all devices
+# TextIOWrapper objects for the two devices
 THUM_devices = [io.TextIOWrapper(io.BufferedRWPair(serial_device, serial_device)) for serial_device in serial_devices]
 
 try:
     while True:
         for i, device_number in enumerate(device_numbers):
             read_device(THUM_devices[i], device_number)
-            sleep(1)  # Add a small delay between devices
 
 except KeyboardInterrupt:
     # Clean up when interrupted
