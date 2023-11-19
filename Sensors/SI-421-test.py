824529f96be404d26385318a964fe1aa02e6d257
AlaminBantan <bantanalamin@gmail.com>
2023-11-19 11:20:36 +0300

    up

--- Sensors/SI-421-test.py
+++ Sensors/SI-421-test.py
@@ -66,11 +66,13 @@ try:
         # command_command_5 is the Slave ID + M!, to take measurement
         command_0 = "0M!\r"
         IR_0.write(command_0)
+        print("take measurement 0")
         IR_0.flush()
         time.sleep(1)
         # read bit
         data_str_0 ="0D0!\r"
         IR_0.write(data_str_0)
+        print("read measurement 0")
         data_0 = IR_0.readline()
         IR_0.flush()
         time.sleep(1)
@@ -86,11 +88,13 @@ try:
         # command_1 is the Slave ID + M!, to take measurement
         command_1 = "1M!\r"
         IR_1.write(command_1)
+        print("take measurement 1")
         IR_1.flush()
         time.sleep(1)
         # read bit
         data_str_1 ="1D0!\r"
         IR_1.write(data_str_1)
+        print("read measurement 1")
         data_1 = IR_1.readline()
         
         IR_1.flush()
@@ -107,12 +111,14 @@ try:
         # command_2 is the Slave ID + M!, to take measurement
         command_2 = "2M!\r"
         IR_2.write(command_2)
+        print("take measurement 2")
         IR_2.flush()
         time.sleep(1)
         # read bit
         data_str_2 ="2D0!\r"
         IR_2.write(data_str_2)
         data_2 = IR_2.readline()
+        print("read measurement 2")
         IR_2.flush()
         time.sleep(1)
         if len(data_2.split('+'))> 1:
@@ -127,12 +133,14 @@ try:
         # command_3 is the Slave ID + M!, to take measurement
         command_3 = "3M!\r"
         IR_3.write(command_3)
+        print("take measurement 3")
         IR_3.flush()
         time.sleep(1)
         # read bit
         data_str ="3D0!\r"
         IR_3.write(data_str)
         data_3 = IR_3.readline()
+        print("read measurement 3")
         IR_3.flush()
         time.sleep(1)
         if len(data_3.split('+'))> 1:
@@ -147,12 +155,14 @@ try:
         # command_4 is the Slave ID + M!, to take measurement
         command_4 = "4M!\r"
         IR_4.write(command_4)
+        print("take measurement 4")
         IR_4.flush()
         time.sleep(1)
         # read bit
         data_str ="4D0!\r"
         IR_4.write(data_str)
         data_4 = IR_4.readline()
+        print("read measurement 4")
         IR_4.flush()
         time.sleep(1)
         if len(data_4.split('+'))> 1:
@@ -168,12 +178,14 @@ try:
         # command_5 is the Slave ID + M!, to take measurement
         command_5 = "5M!\r"
         IR_5.write(command_5)
+        print("take measurement 5")
         IR_5.flush()
         time.sleep(1)
         # read bit
         data_str ="5D0!\r"
         IR_5.write(data_str)
         data_5 = IR_5.readline()
+        print("read measurement 5")
         IR_5.flush()
         time.sleep(1)
         if len(data_5.split('+'))> 1:
