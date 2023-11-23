ab11ca691e0d8420d8561b8765d679397f8c849f
AlaminBantan <bantanalamin@gmail.com>
2023-11-23 10:57:23 +0300

    up

--- Sensors/SI-421-0.py
+++ Sensors/SI-421-0.py
@@ -1,31 +1,36 @@
 import serial
 import time
 import io
-from datetime import datetime
 
-IR_0 = serial.Serial("/dev/ttyACM0",
+IR_0 = serial.Serial("/dev/ttyUSB0",
                    baudrate=9600,
                    bytesize=serial.EIGHTBITS,
                    parity=serial.PARITY_NONE,
                    stopbits=serial.STOPBITS_ONE,
                    xonxoff=False,
-                   timeout=1)
+                   timeout=2)
 IR_0 = io.TextIOWrapper(io.BufferedRWPair(IR_0, IR_0))
 try:
     while True:
         # command_command_5 is the Slave ID + M!, to take measurement
-        command_0 = "0M!\r"
-        IR_0.write(command_0)
+        IR_0.write("0M!\r")
+        print("send")
         IR_0.flush()
         time.sleep(1)
         # read bit
-        data_str_0 ="0D0!\r"
-        IR_0.write(data_str_0)
-        data_0 = IR_0.readline()
+
+        IR_0.write("0D0!\r")
+        print("read")
         IR_0.flush()
         time.sleep(1)
-        if len(data_0.split('+'))> 1:
-            print(f"Temperature of the surface 0 is: {data_0.split('+')[1]} degrees celcius")
+
+        data_0 = IR_0.readlines()
+        print("print data")
+        IR_0.flush()
+        print(data_0)
+        time.sleep(1)
+
+
 except KeyboardInterrupt:
     # Clean up when interrupted
     print("Ports Now Closed")
