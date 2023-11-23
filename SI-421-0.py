8fc68e6ca86f57ac477ddbc6f0b0b9492bbb2028
AlaminBantan <bantanalamin@gmail.com>
2023-11-23 11:37:25 +0300

    up

--- SI-421-0.py
+++ SI-421-0.py
@@ -1,6 +1,7 @@
 import serial
 import time
 import io
+from datetime import datetime
 
 IR_0 = serial.Serial("/dev/ttyUSB0",
                    baudrate=9600,
@@ -8,29 +9,23 @@ IR_0 = serial.Serial("/dev/ttyUSB0",
                    parity=serial.PARITY_NONE,
                    stopbits=serial.STOPBITS_ONE,
                    xonxoff=False,
-                   timeout=2)
-IR_0 = io.TextIOWrapper(io.BufferedRWPair(IR_0, IR_0))
+                   timeout=1)
+IR_01 = io.TextIOWrapper(io.BufferedRWPair(IR_0, IR_0))
 try:
     while True:
         # command_command_5 is the Slave ID + M!, to take measurement
-        IR_0.write("0M!\r")
-        print("send")
-        IR_0.flush()
+        command_0 = "0M!\r"
+        IR_01.write(command_0)
+        IR_01.flush()
         time.sleep(1)
         # read bit
-
-        IR_0.write("0D0!\r")
-        print("read")
-        IR_0.flush()
+        data_str_0 ="0D0!\r"
+        IR_01.write(data_str_0)
+        data_0 = IR_01.readlines()
+        IR_01.flush()
         time.sleep(1)
-
-        data_0 = IR_0.readlines()
-        print("print data")
-        IR_0.flush()
         print(data_0)
         time.sleep(1)
-
-
 except KeyboardInterrupt:
     # Clean up when interrupted
     print("Ports Now Closed")
