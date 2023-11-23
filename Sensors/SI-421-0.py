56726c1961250a1d1bec85a0c70d2892a2563190
AlaminBantan <bantanalamin@gmail.com>
2023-11-23 11:39:01 +0300

    up

--- Sensors/SI-421-0.py
+++ Sensors/SI-421-0.py
@@ -16,12 +16,14 @@ try:
         # command_command_5 is the Slave ID + M!, to take measurement
         command_0 = "0M!\r"
         IR_01.write(command_0)
+        print("send")
         IR_01.flush()
         time.sleep(1)
         # read bit
         data_str_0 ="0D0!\r"
         IR_01.write(data_str_0)
         data_0 = IR_01.readlines()
+        print("read")
         IR_01.flush()
         time.sleep(1)
         print(data_0)
