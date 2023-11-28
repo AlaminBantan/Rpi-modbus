4d7e1713782ccf1b6c583bd870d7044ef9229988
AlaminBantan <bantanalamin@gmail.com>
2023-11-28 14:50:21 +0300

    up

---
+++ Sensors/SI-421-troublesooting.py
@@ -0,0 +1,28 @@
+import serial
+import time
+import io
+
+
+se = serial.Serial("/dev/ttyUSB0",
+                baudrate=9600,
+                bytesize=serial.EIGHTBITS,
+                parity=serial.PARITY_NONE,
+                stopbits=serial.STOPBITS_ONE,
+                xonxoff=False,
+                timeout=1)
+
+sio = io.TextIOWrapper(io.BufferedRWPair(se,se))
+
+try:
+    while True:
+    #Command is the Slave ID + M!, to take measurement
+        command = "?1"
+        print(f"{command} sent")
+        sio.write(command)
+        sio.flush()
+        time.sleep(0.2)
+        data = sio.readlines()
+        print(data)
+        time.sleep(2)
+except KeyboardInterrupt
+print("closed")
