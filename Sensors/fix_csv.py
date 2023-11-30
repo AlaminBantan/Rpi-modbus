40a51a91bcc1ffe78cbf5522e50b9a10f52c0d5c
AlaminBantan <bantanalamin@gmail.com>
2023-11-30 15:02:40 +0300

    up

--- Sensors/fix_csv.py
+++ Sensors/fix_csv.py
@@ -1,37 +1,28 @@
-import pandas as pd
+import csv
 
-# Assuming 'C:\\Users\\bantanam\\Downloads\\Light_comparison.csv' is the file containing your data
-df = pd.read_csv('C:\\Users\\bantanam\\Downloads\\Light_comparison11.csv')
+# Input file path
+file_path = '/home/cdacea/GH_data/climatic_data.csv'
 
-# Convert 'Date' and 'Time' columns to datetime format
-df['Date'] = pd.to_datetime(df['Date'])
-df['Time'] = pd.to_datetime(df['Time'], format='%H:%M').dt.time
+# Read the CSV file and remove duplicate headers
+with open(file_path, 'r+', newline='') as file:
+    reader = csv.reader(file)
+    writer = csv.writer(file)
 
-# Create separate columns for date and time
-df['Date'] = df['Date'].dt.date
-df['Time'] = df['Time'].apply(lambda x: x.strftime('%H:%M'))
+    # Flag to skip the first header
+    skip_header = True
 
-# Replace negative values with 0 in specific columns
-cols_to_replace = ['PAR Intensity Zone B (1)', 'PAR Intensity Zone C (1)', 'Solar Radiation Zone B (1)', 'Solar Radiation Zone C (1)', 'PAR Intensity Zone B (2)', 'PAR Intensity Zone C (2)', 'Solar Radiation Zone B (2)', 'Solar Radiation Zone C (2)', 'PAR Intensity Zone B (3)', 'PAR Intensity Zone C (3)', 'Solar Radiation Zone B (3)', 'Solar Radiation Zone C (3)']
-df[cols_to_replace] = df[cols_to_replace].apply(lambda x: x.clip(lower=0))
+    # Store the lines to rewrite the file if needed
+    lines_to_write = []
 
+    for row in reader:
+        if not skip_header:
+            lines_to_write.append(row)
+        else:
+            skip_header = False
 
-# Group by minute and aggregate the values
-df_minute = df.groupby(['Date', 'Time']).agg({
-    'PAR Intensity Zone B (1)': 'mean',
-    'PAR Intensity Zone B (2)': 'mean',
-    'PAR Intensity Zone B (3)': 'mean',
-    'PAR Intensity Zone C (1)': 'mean',
-    'PAR Intensity Zone C (2)': 'mean',
-    'PAR Intensity Zone C (3)': 'mean',
-    'Solar Radiation Zone B (1)': 'mean',
-    'Solar Radiation Zone B (2)': 'mean',
-    'Solar Radiation Zone B (3)': 'mean',
-    'Solar Radiation Zone C (1)': 'mean',
-    'Solar Radiation Zone C (2)': 'mean',
-    'Solar Radiation Zone C (3)': 'mean',
+    # Move the file cursor to the beginning
+    file.seek(0)
+    file.truncate()
 
-}).reset_index()
-
-# Save the consolidated data as a CSV file
-df_minute.to_csv('C:\\Users\\bantanam\\Downloads\\Light_comparison_28_September.csv', index=False)
+    # Write the remaining rows without duplicate headers
+    writer.writerows(lines_to_write)
