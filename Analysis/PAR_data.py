ca8b742c8b29d4fa7cd837432e5ecdb005760b89
AlaminBantan <bantanalamin@gmail.com>
2023-12-04 14:44:44 +0300

    up

--- Analysis/PAR_data.py
+++ Analysis/PAR_data.py
@@ -13,12 +13,15 @@ df_pivoted = df_extracted.pivot_table(index=['RoundedDateTime'], columns=['Zone'
 df_pivoted['Mean zone B'] = (df_pivoted[('B', 1)] + df_pivoted[('B', 2)]) / 2
 df_pivoted['Mean zone C'] = (df_pivoted[('C', 1)] + df_pivoted[('C', 2)]) / 2
 
-# Reset the index to have 'date' and 'time' as separate columns
+# Reset the index to have 'date' as a separate column
 df_pivoted.reset_index(inplace=True)
 df_pivoted.columns = ['date', 'Zone B subzone 1', 'Zone B subzone 2', 'Zone C subzone 1', 'Zone C subzone 2', 'Mean zone B', 'Mean zone C']
 
 # Split 'date' column into 'date' and 'time'
 df_pivoted[['date', 'time']] = df_pivoted['date'].str.split(expand=True)
+
+# Rearrange columns to have 'time' next to 'date'
+df_pivoted = df_pivoted[['date', 'time', 'Zone B subzone 1', 'Zone B subzone 2', 'Zone C subzone 1', 'Zone C subzone 2', 'Mean zone B', 'Mean zone C']]
 
 # Save the result to a new CSV file
 df_pivoted.to_csv('/home/cdacea/GH_data/mean_PAR_data.csv', index=False)
