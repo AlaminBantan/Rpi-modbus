4bd032fdf4e67b2c96b336965e5e7c44391ee546
AlaminBantan <bantanalamin@gmail.com>
2023-12-10 13:20:55 +0300

    up

--- Analysis/Temp_data.py
+++ Analysis/Temp_data.py
@@ -1,13 +1,14 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Read the CSV file
 df = pd.read_csv('C:\\Users\\bantanam\\KAUST\\CDA-CEA Team - Documents\\CO2 misting - Cucumber trial\\Data collection\\Climatic_data\\modified_data_15min.csv')
 
 # Extract relevant columns
-df_extracted = df[['RoundedDateTime', 'Zone', 'Subzone', 'PAR']]
+df_extracted = df[['RoundedDateTime', 'Zone', 'Subzone', 'Temp']]
 
 # Pivot the table to have zones and subzones as columns
-df_pivoted = df_extracted.pivot_table(index=['RoundedDateTime'], columns=['Zone', 'Subzone'], values='PAR')
+df_pivoted = df_extracted.pivot_table(index=['RoundedDateTime'], columns=['Zone', 'Subzone'], values='Temp')
 
 # Calculate mean values for each zone
 df_pivoted['Mean zone B'] = (df_pivoted[('B', 1)] + df_pivoted[('B', 2)]) / 2
@@ -30,4 +31,6 @@ df_pivoted[['date', 'time']] = df_pivoted['date'].str.split(expand=True)
 df_pivoted = df_pivoted[['date', 'time', 'Zone B subzone 1', 'Zone B subzone 2', 'Zone C subzone 1', 'Zone C subzone 2', 'Mean zone B', 'Mean zone C']]
 
 # Save the result to a new CSV file
-df_pivoted.to_csv('C:\\Users\\bantanam\\KAUST\\CDA-CEA Team - Documents\\CO2 misting - Cucumber trial\\Data collection\\Climatic_data\\mean_PAR_data.csv', index=False)
+df_pivoted.to_csv('C:\\Users\\bantanam\\KAUST\\CDA-CEA Team - Documents\\CO2 misting - Cucumber trial\\Data collection\\Climatic_data\\mean_Temperature_data.csv', index=False)
+
+
