6a36024c2f3253e5b4926cd9ef98addb5716a5b4
AlaminBantan <bantanalamin@gmail.com>
2024-01-17 14:11:59 +0300

    up

--- Mini_GH/north_GH.py
+++ Mini_GH/north_GH.py
@@ -4,27 +4,27 @@ import datetime
 import csv
 import os
 
-# Configuration of SQ-618 ID=6
-PAR_6 = minimalmodbus.Instrument('/dev/ttyACM0', 6)
-PAR_6.serial.baudrate = 19200
-PAR_6.serial.bytesize = 8
-PAR_6.serial.parity = minimalmodbus.serial.PARITY_EVEN
-PAR_6.serial.stopbits = 1
-PAR_6.serial.timeout = 0.5
-PAR_6.mode = minimalmodbus.MODE_RTU
-PAR_6.clear_buffers_before_each_transaction = True
-PAR_6.close_port_after_each_call = True
+# Configuration of SQ-618 ID=5
+PAR_5 = minimalmodbus.Instrument('/dev/ttyACM0', 5)
+PAR_5.serial.baudrate = 19200
+PAR_5.serial.bytesize = 8
+PAR_5.serial.parity = minimalmodbus.serial.PARITY_EVEN
+PAR_5.serial.stopbits = 1
+PAR_5.serial.timeout = 0.5
+PAR_5.mode = minimalmodbus.MODE_RTU
+PAR_5.clear_buffers_before_each_transaction = True
+PAR_5.close_port_after_each_call = True
 
 # Configuration of SP-522 ID=10
-Solar_10 = minimalmodbus.Instrument('/dev/ttyACM0', 10)
-Solar_10.serial.baudrate = 19200
-Solar_10.serial.bytesize = 8
-Solar_10.serial.parity = minimalmodbus.serial.PARITY_EVEN
-Solar_10.serial.stopbits = 1
-Solar_10.serial.timeout = 0.5
-Solar_10.mode = minimalmodbus.MODE_RTU
-Solar_10.clear_buffers_before_each_transaction = True
-Solar_10.close_port_after_each_call = True
+Solar_15 = minimalmodbus.Instrument('/dev/ttyACM0', 15)
+Solar_15.serial.baudrate = 19200
+Solar_15.serial.bytesize = 8
+Solar_15.serial.parity = minimalmodbus.serial.PARITY_EVEN
+Solar_15.serial.stopbits = 1
+Solar_15.serial.timeout = 0.5
+Solar_15.mode = minimalmodbus.MODE_RTU
+Solar_15.clear_buffers_before_each_transaction = True
+Solar_15.close_port_after_each_call = True
 
 
 
@@ -54,10 +54,10 @@ try:
             date, time = get_datetime()
 
             try:
-                # Read data from PAR_6
-                PAR_intensity_6 = PAR_6.read_float(0, 3, 2, 0)
+                # Read data from PAR_5
+                PAR_intensity_5 = PAR_5.read_float(0, 3, 2, 0)
                 sleep(5)
-                writer.writerow({'Date': date, 'Time': time, 'PAR_south': PAR_intensity_6})
+                writer.writerow({'Date': date, 'Time': time, 'PAR_south': PAR_intensity_5})
                 sleep(5)
             except Exception as e:
                 now = get_datetime()
@@ -65,22 +65,22 @@ try:
 
 
             try:
-                # Read data from Solar_10
-                Solar_Radiation_10 = Solar_10.read_float(0, 3, 2, 0)
+                # Read data from Solar_15
+                Solar_Radiation_15 = Solar_15.read_float(0, 3, 2, 0)
                 sleep(5)
-                writer.writerow({'Date': date, 'Time': time, 'Solar_radiation_south': Solar_Radiation_10})
+                writer.writerow({'Date': date, 'Time': time, 'Solar_radiation_south': Solar_Radiation_15})
                 sleep(5)
             except Exception as e:
                 now = get_datetime()
-                print(f"Error reading Solar_10 at {now[1]} on {now[0]}: {e}")
+                print(f"Error reading Solar_15 at {now[1]} on {now[0]}: {e}")
 
 
 except KeyboardInterrupt:
     # Close serial ports only if they are open
-    if Solar_10.serial.is_open:
-        Solar_10.serial.close()
+    if Solar_15.serial.is_open:
+        Solar_15.serial.close()
 
-    if PAR_6.serial.is_open:
-        PAR_6.serial.close()
+    if PAR_5.serial.is_open:
+        PAR_5.serial.close()
 
     print("Ports Closed")
