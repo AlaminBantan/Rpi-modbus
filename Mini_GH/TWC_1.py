6e2db28271248153283b3c32ecf255cf6291c7dd
AlaminBantan <bantanalamin@gmail.com>
2024-02-12 14:37:31 +0300

    up

--- Mini_GH/TWC_1.py
+++ Mini_GH/TWC_1.py
@@ -1,45 +1,25 @@
 import minimalmodbus
 from time import sleep
 
-def configure_instrument(port, address):
-    instrument = minimalmodbus.Instrument(port, address)
-    instrument.serial.baudrate = 9600  
-    instrument.serial.bytesize = 8  
-    instrument.serial.parity = minimalmodbus.serial.PARITY_NONE  
-    instrument.serial.stopbits = 1  
-    instrument.mode = minimalmodbus.MODE_RTU
-    instrument.clear_buffers_before_each_transaction = True
-    instrument.close_port_after_each_call = True
-    return instrument
+TWC_1 = minimalmodbus.TWC_1('/dev/ttyACM0', 0)
+TWC_1.serial.baudrate = 9600  
+TWC_1.serial.bytesize = 8 
+TWC_1.serial.parity = minimalmodbus.serial.PARITY_NONE  
+TWC_1.serial.stopbits = 1  
+TWC_1.mode = minimalmodbus.MODE_RTU
+TWC_1.clear_buffers_before_each_transaction = True
+TWC_1.close_port_after_each_call = True
 
-def read_sensor_data(instrument):
-    try:
-        temp = instrument.read_register(0, 0, 4, False)
-        water_content = instrument.read_register(1, 0, 4, False)
-        ec = instrument.read_register(3, 0, 4, False)
-        salinity = instrument.read_register(4, 0, 4, False)
-        tds = instrument.read_register(5, 0, 4, False)
 
-        return temp, water_content, ec, salinity, tds
-    except Exception as e:
-        print(f"Error reading sensor data: {e}")
-        return None
 
-if __name__ == "__main__":
-    instrument_1 = configure_instrument('/dev/ttyACM0', 0)
+try:
+    while True:
+        temp_1 = TWC_1.read_register(0, 0, 3, False)
+        print(f"Temp: {temp_1}")
+        sleep(1)
 
-    try:
-        while True:
-            sensor_data = read_sensor_data(instrument_1)
-            if sensor_data is not None:
-                temp, water_content, ec, salinity, tds = sensor_data
-                print(f"Temp: {temp}, Water Content: {water_content}, EC: {ec}, Salinity: {salinity}, TDS: {tds}")
-            sleep(1)  # Add a sleep time to avoid high CPU usage
-
-    except KeyboardInterrupt:
+except KeyboardInterrupt:
         print("KeyboardInterrupt: Closing port.")
-    except Exception as e:
+except Exception as e:
         print(f"An unexpected error occurred: {e}")
-    finally:
-        instrument_1.serial.close()
-        print("Port now closed.")
+
