c887b481d9d9667b604108b8e7d33e4764fd4326
AlaminBantan <bantanalamin@gmail.com>
2024-02-14 15:37:02 +0300

    up

--- Mini_GH/TWC_1.py
+++ Mini_GH/TWC_1.py
@@ -1,7 +1,7 @@
 import minimalmodbus
 from time import sleep
 
-TWC_1 = minimalmodbus.Instrument('/dev/ttyACM0', 0)
+TWC_1 = minimalmodbus.Instrument('/dev/ttyACM0', 1)
 TWC_1.serial.baudrate = 9600  
 TWC_1.serial.bytesize = 8 
 TWC_1.serial.parity = minimalmodbus.serial.PARITY_NONE  
@@ -9,6 +9,8 @@ TWC_1.serial.stopbits = 1
 TWC_1.mode = minimalmodbus.MODE_RTU
 TWC_1.clear_buffers_before_each_transaction = True
 TWC_1.close_port_after_each_call = True
+
+
 
 try:
     while True:
