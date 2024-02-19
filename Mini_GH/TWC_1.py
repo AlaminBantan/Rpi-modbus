ee11d0a7a795bf7d00c341a3ab64f3ade5de3184
AlaminBantan <bantanalamin@gmail.com>
2024-02-19 13:57:47 +0300

    UP

--- Mini_GH/TWC_1.py
+++ Mini_GH/TWC_1.py
@@ -11,9 +11,9 @@ TWC_1.clear_buffers_before_each_transaction = True
 TWC_1.close_port_after_each_call = True
 
 #Change the slave address to 2
-#new_slave_address = 2
-#TWC_1.write_register(512, new_slave_address, functioncode=6, number_of_decimals=0, signed=False)
-#print("Successfully changed the slave address to:", new_slave_address)
+new_slave_address = 4
+TWC_1.write_register(512, new_slave_address, functioncode=6, number_of_decimals=0, signed=False)
+
 
 #try:
 #   while True:
@@ -26,7 +26,7 @@ TWC_1.close_port_after_each_call = True
 #   print("TWC_1 closed")
 
 # Read the temperature register
-temperature_raw = TWC_1.read_register(0, functioncode=3, number_of_decimals=0, signed=True)
-temperature_celsius = temperature_raw / 100.0  # Convert to Celsius
+#temperature_raw = TWC_1.read_register(0, functioncode=3, number_of_decimals=0, signed=True)
+#temperature_celsius = temperature_raw / 100.0  # Convert to Celsius
 
-print("Temperature:", temperature_celsius, "°C")
+#print("Temperature:", temperature_celsius, "°C")
