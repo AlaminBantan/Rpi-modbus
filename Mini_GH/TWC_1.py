d5711a61c5f901a72e62f1e3dac3961c566bfe0e
AlaminBantan <bantanalamin@gmail.com>
2024-02-19 13:45:43 +0300

    up

--- Mini_GH/TWC_1.py
+++ Mini_GH/TWC_1.py
@@ -1,7 +1,7 @@
 import minimalmodbus
 from time import sleep
 
-TWC_1 = minimalmodbus.Instrument('/dev/ttyACM0', 18)
+TWC_1 = minimalmodbus.Instrument('/dev/ttyACM0', 0)
 TWC_1.serial.baudrate = 9600  
 TWC_1.serial.bytesize = 8 
 TWC_1.serial.parity = minimalmodbus.serial.PARITY_NONE  
@@ -10,13 +10,17 @@ TWC_1.mode = minimalmodbus.MODE_RTU
 TWC_1.clear_buffers_before_each_transaction = True
 TWC_1.close_port_after_each_call = True
 
+# Change the slave address to 2
+new_slave_address = 2
+TWC_1.write_register(512, new_slave_address, functioncode=6, number_of_decimals=0, signed=False)
+print("Successfully changed the slave address to:", new_slave_address)
 
-try:
-   while True:
-    temp_1=TWC_1.read_register(0, 0, 3 ,False)
-    print(f"Temp is {temp_1}")
-    sleep(1)
+#try:
+#   while True:
+#    temp_1=TWC_1.read_register(0, 0, 3 ,False)
+#    print(f"Temp is {temp_1}")
+#    sleep(1)
 
-except KeyboardInterrupt:
-   TWC_1.serial.close()
-   print("TWC_1 closed")
+#except KeyboardInterrupt:
+#   TWC_1.serial.close()
+#   print("TWC_1 closed")
