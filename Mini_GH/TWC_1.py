32747673adfe469b9ce68fb44a92f20cb3c04167
AlaminBantan <bantanalamin@gmail.com>
2024-02-20 10:31:35 +0300

    up

--- Mini_GH/TWC_1.py
+++ Mini_GH/TWC_1.py
@@ -1,7 +1,7 @@
 import minimalmodbus
 from time import sleep
 
-TWC_1 = minimalmodbus.Instrument('/dev/ttyACM0', 4)
+TWC_1 = minimalmodbus.Instrument('/dev/ttyACM0', 1)
 TWC_1.serial.baudrate = 9600  
 TWC_1.serial.bytesize = 8 
 TWC_1.serial.parity = minimalmodbus.serial.PARITY_NONE  
@@ -17,7 +17,7 @@ TWC_1.close_port_after_each_call = True
 
 try:
    while True:
-    temp_1=TWC_1.read_register(0, 0, 3 ,False)
+    temp_1=TWC_1.read_register(0, number_of_decimals=0, functioncode= 3 ,signed=False)
     print(f"Temp is {temp_1}")
     sleep(1)
 
