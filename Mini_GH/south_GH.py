d89ff2bec58decad8a8b414cb0df8960c16dd362
AlaminBantan <bantanalamin@gmail.com>
2024-03-24 16:04:51 +0300

    up

--- Mini_GH/south_GH.py
+++ Mini_GH/south_GH.py
@@ -2,85 +2,122 @@ import minimalmodbus
 from time import sleep
 import datetime
 import csv
+import serial
+import io
 import os
 
-# Configuration of SQ-618 ID=6
-PAR_6 = minimalmodbus.Instrument('/dev/ttyACM0', 6)
-PAR_6.serial.baudrate = 19200
-PAR_6.serial.bytesize = 8
-PAR_6.serial.parity = minimalmodbus.serial.PARITY_EVEN
-PAR_6.serial.stopbits = 1
-PAR_6.serial.timeout = 0.5
-PAR_6.mode = minimalmodbus.MODE_RTU
-PAR_6.clear_buffers_before_each_transaction = True
-PAR_6.close_port_after_each_call = True
+# Configuration of SQ-618 ID=5
+PAR_2 = minimalmodbus.Instrument('/dev/ttyACM0', 1)
+PAR_2.serial.baudrate = 19200
+PAR_2.serial.bytesize = 8
+PAR_2.serial.parity = minimalmodbus.serial.PARITY_EVEN
+PAR_2.serial.stopbits = 1
+PAR_2.serial.timeout = 0.5
+PAR_2.mode = minimalmodbus.MODE_RTU
+PAR_2.clear_buffers_before_each_transaction = True
+PAR_2.close_port_after_each_call = True
 
 # Configuration of SP-522 ID=10
-Solar_10 = minimalmodbus.Instrument('/dev/ttyACM0', 10)
-Solar_10.serial.baudrate = 19200
-Solar_10.serial.bytesize = 8
-Solar_10.serial.parity = minimalmodbus.serial.PARITY_EVEN
-Solar_10.serial.stopbits = 1
-Solar_10.serial.timeout = 0.5
-Solar_10.mode = minimalmodbus.MODE_RTU
-Solar_10.clear_buffers_before_each_transaction = True
-Solar_10.close_port_after_each_call = True
+Solar_12 = minimalmodbus.Instrument('/dev/ttyACM0', 11)
+Solar_12.serial.baudrate = 19200
+Solar_12.serial.bytesize = 8
+Solar_12.serial.parity = minimalmodbus.serial.PARITY_EVEN
+Solar_12.serial.stopbits = 1
+Solar_12.serial.timeout = 0.5
+Solar_12.mode = minimalmodbus.MODE_RTU
+Solar_12.clear_buffers_before_each_transaction = True
+Solar_12.close_port_after_each_call = True
 
 
+# Configuration of GMP-252 ID=41
+carbo_42 = minimalmodbus.Instrument('/dev/ttyACM0',41)
+carbo_42.serial.baudrate = 19200
+carbo_42.serial.bytesize = 8
+carbo_42.serial.parity = minimalmodbus.serial.PARITY_NONE
+carbo_42.serial.stopbits = 2
+carbo_42.mode = minimalmodbus.MODE_RTU
+carbo_42.clear_buffers_before_each_transaction = True
+carbo_42.close_port_after_each_call = True
+
+# Configuration of HMP-155
+serial_THUM = serial.Serial("/dev/ttyACM1",
+                   baudrate=4800,
+                  bytesize=serial.SEVENBITS,
+                   parity=serial.PARITY_EVEN,
+                   stopbits=serial.STOPBITS_ONE,
+                   xonxoff=False,
+                   timeout=2)
+THUM_32 = io.TextIOWrapper(io.BufferedRWPair(serial_THUM, serial_THUM))
 
 # Define a function to get the current date and time in the required format
 def get_datetime():
-    now = datetime.datetime.now()
-    return now.strftime("%m/%d/%Y"), now.strftime("%H:%M")
-
-# Define the file path for the CSV file
-Climatic_data_pathway = "/home/cdacea/south_GH/south_climatic_data.csv"
-
-# Check if the file is empty
-file_exists = os.path.exists(Climatic_data_pathway) and os.path.getsize(Climatic_data_pathway) > 0
-
+    timenow = datetime.datetime.now()
+    return timenow
 try:
-    with open(Climatic_data_pathway, mode='a', newline='') as csv_file:
-        fieldnames = ['Date', 'Time', 'PAR_south', 'Solar_radiation_south']
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    while True:
+        current_datetime = get_datetime()
 
-        # Write the header only if the file is empty
-        if not file_exists:
-            writer.writeheader()
+        # Format date and time without decimal seconds
+        formatted_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
 
+        try:
+            # Read data from PAR_2 and round to 1 decimal place
+            PAR_intensity_1 = round(PAR_2.read_float(0, 3, 2, 0), 1)
+        except Exception as e:
+            PAR_intensity_1 = "Error"
 
+        try:
+            # Read data from Solar_12 and round to 1 decimal place
+            Solar_Radiation_11 = round(Solar_12.read_float(0, 3, 2, 0), 1)
+        except Exception as e:
+            Solar_Radiation_11 = "Error"
 
-        while True:
-            date, time = get_datetime()
+        try:
+            # Read data from carbo_42 and round to 1 decimal place
+            carbon_conc_41 = round(carbo_42.read_float(1, 3, 2, 0), 1)
+        except Exception as e:
+            carbon_conc_41 = "Error"
 
-            try:
-                # Read data from PAR_6
-                PAR_intensity_6 = PAR_6.read_float(0, 3, 2, 0)
-                sleep(5)
-                writer.writerow({'Date': date, 'Time': time, 'PAR_south': PAR_intensity_6})
-                sleep(5)
-            except Exception as e:
-                now = get_datetime()
-                print(f"Error reading PAR_5 at {now[1]} on {now[0]}: {e}")
+        # Read data from THUM_32
+        try:
+            THUM_32.write("OPEN 31\r\n")
+            THUM_32.flush()
+            sleep(1)
+            THUM_32.write("SEND\r\n")
+            THUM_32.flush()
+            sleep(1)
+            data_32 = THUM_32.readlines()
+            last_line_32 = data_32[-1]
+            rh_index_32 = last_line_32.find('RH=')
+            temp_index_32 = last_line_32.find("Ta=")
+            if rh_index_32 != -1 and temp_index_32 != -1:
+                rh_value_32 = float(last_line_32[rh_index_32 + 3:last_line_32.find('%RH')])
+                temp_value_31 = float(last_line_32[temp_index_32 + 3:last_line_32.find("'C")])
+        except Exception as e:
+            rh_value_32 = "Error"
+            temp_value_32 = "Error"
 
+        # Print the sensor readings
+        print("\n"*10)
+        print("--------------------------------")
+        print("Climatic condition in North GH!")
+        print(f"Time is {formatted_datetime}")
+        print(f"PAR is {PAR_intensity_1} umol.m-2.s-1")
+        print(f"Solar radiation is {Solar_Radiation_11} W.m-2")
+        print(f"Temperature is {temp_value_31:.1f} C")
+        print(f"Relative humidity is {rh_value_31}%")
+        print(f"Carbon concentration is {carbon_conc_41} ppm")
+        print("--------------------------------")
 
-            try:
-                # Read data from Solar_10
-                Solar_Radiation_10 = Solar_10.read_float(0, 3, 2, 0)
-                sleep(5)
-                writer.writerow({'Date': date, 'Time': time, 'Solar_radiation_south': Solar_Radiation_10})
-                sleep(5)
-            except Exception as e:
-                now = get_datetime()
-                print(f"Error reading Solar_10 at {now[1]} on {now[0]}: {e}")
-
+        sleep(10)
 
 except KeyboardInterrupt:
     # Close serial ports only if they are open
-    if Solar_10.serial.is_open:
-        Solar_10.serial.close()
-
-    if PAR_6.serial.is_open:
-        PAR_6.serial.close()
+    if PAR_2.serial.is_open:
+        PAR_2.serial.close()
+    if Solar_12.serial.is_open:
+        Solar_12.serial.close()
+    if carbo_42.serial.is_open:
+        carbo_42.serial.close()
 
     print("Ports Closed")
